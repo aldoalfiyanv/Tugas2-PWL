576f945e49b4c1c5106b19675e4f8a4c0926c028
Aldo Alfiyan Vananda <86955469+aldoalfiyanv@users.noreply.github.com>
2022-06-21 23:26:54 +0700

    First Commit
    First Commit

---
+++ operator_pembanding.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Tugas 2</title>
+</head>
+<body>
+        <!-- Navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Tugas II PWL</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Menu
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/constant.php">Constanta</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/do_while.php">Do While</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/for.php">For</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/foreach.php">Foreach</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/hello_world.php">Hello World</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/if.php">If</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/if_else.php">If Else</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/operator_aritmatika.php">Operator Aritmatika</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/operator_pembanding.php">Operator Pembanding</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/operator_string.php">Operator String</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/switch.php">Switch</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/variable.php">Variable</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/variable_object.php">Variable Object</a></li>
+                    <li><a class="dropdown-item" href="http://localhost/Y6E-202143579015/while.php">While</a></li>
+                </ul>
+                </li>
+            </ul>
+        </div>
+        </nav>
+
+        <header class="container bg-secondary text-white">
+            <div class="row">
+                <div class="col-12 py-4 text-center">
+                    <h1 class="font-monospace">202143579015 - Aldo Alfiyan V </h1>
+                    <p class="lead">Operator Pembanding</p>
+                </div>
+            </div>
+        </header>
+
+        <!-- Perintah PHP -->
+        <main class="container border">
+            <div class="row">
+                <div class="col-12 py-5 text-center">
+                    <?php 
+                       $x = 100;
+                       $y = 50;
+
+                       echo "Hasil $x > $y = ", var_dump($x > $y),"<br>";
+                       echo "Hasil $x < $y = ", var_dump($x < $y),"<br>";
+                       echo "Hasil $x = $y = ", var_dump($x == $y),"<br>";
+                       echo "Hasil $x != $y = ", var_dump($x != $y),"<br>";
+                    ?>
+                </div>
+            </div>
+        </main>
+
+        <!-- Footer -->
+        <footer>
+            <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0);">
+            Tugas 2 Pemrograman Web © 2022 Copyright:
+            <a class="text-dark" href="#">Aldo Alfiyan Vananda</a>
+            </div>
+        </footer>
+</body>
+</html>
